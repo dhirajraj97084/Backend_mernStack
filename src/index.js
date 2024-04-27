3f930d144116afdd18e4b599802d03894a878191
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-04-27 12:26:45 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 
 import express from 'express';
 import connectDB from './db/database.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 
 const app=express();
 const port=process.env.PORT||8000;
+// middleware
+app.use(cors({
+    origin:process.env.COURCE_ORIGIN
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 connectDB();
-
-
 
 app.listen(port,()=>{
     console.log(`your server start localhost:${port}`)
